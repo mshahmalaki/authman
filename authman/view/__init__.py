e82864c7697a91c417e5dcdb01a768eedd1250e2
mshahmalaki <mshahmalaki@gmail.com>
2020-10-03 02:12:49 +0330

    Create UserResource in view

---
+++ authman/view/__init__.py
@@ -0,0 +1,14 @@
+from authman import api
+from authman.view.user import UserResource
+
+
+api.add_resource(
+    UserResource,
+    "/users",
+    methods=["GET", "POST"]
+)
+api.add_resource(
+    UserResource,
+    "users/<int:user_id>",
+    methods=["GET", "PATCH", "DELETE"]
+)
---
+++ authman/view/user.py
@@ -0,0 +1,15 @@
+from flask_restx import Resource
+
+
+class UserResource(Resource):
+    def get(self, user_id=None):
+        pass
+
+    def post(self, user_id=None):
+        pass
+
+    def patch(self, user_id=None):
+        pass
+
+    def delete(self, user_id=None):
+        pass
