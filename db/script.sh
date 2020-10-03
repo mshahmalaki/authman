ede7b20f7dcc0f56bf64732e2e138ac85c8d6b29
mshahmalaki <mshahmalaki@gmail.com>
2020-10-04 03:04:19 +0330

    Add script to run MySQL instance in Docker Environment

--- db/script.sh
+++ db/script.sh
@@ -5,4 +5,4 @@ docker run --rm -d --name authmandb \
 -e MYSQL_USER=authman \
 -e MYSQL_PASSWORD=123456 \
 -p 3316:3306 mysql
-export AUTHMAN_DATABASE_URI=mysql+pymysql://authman@123456@192.168.99.102:3316/authman
+export AUTHMAN_DATABASE_URI=mysql+pymysql://authman:123456@192.168.99.102:3316/authman
