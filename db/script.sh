b8bb12e92e0f859f9cd24ba281d06fb6e850bc23
mshahmalaki <mshahmalaki@gmail.com>
2020-10-04 04:01:05 +0330

    improve bash script

--- db/script.sh
+++ db/script.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+ROOT_PASSWORD="123456"
+DATABASE="authman"
+USER="authman"
+PASSWORD="123456"
+
 docker run --rm -d --name authmandb \
--e MYSQL_ROOT_PASSWORD=123456 \
--e MYSQL_DATABASE=authman \
--e MYSQL_USER=authman \
--e MYSQL_PASSWORD=123456 \
+-e MYSQL_ROOT_PASSWORD=$ROOT_PASSWORD \
+-e MYSQL_DATABASE=$DATABASE \
+-e MYSQL_USER=$USER \
+-e MYSQL_PASSWORD=$PASSWORD \
 -p 3316:3306 mysql
-export AUTHMAN_DATABASE_URI=mysql+pymysql://authman:123456@192.168.99.102:3316/authman
+
+export AUTHMAN_DATABASE_URI=mysql+pymysql://$USER:$PASSWORD@192.168.99.102:3316/$DATABASE
