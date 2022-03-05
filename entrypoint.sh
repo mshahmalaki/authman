c8c8bff5da83fac5285f10a0835b0df5b830cfee
Mohammad Shahmaleki <mshahmalaki@gmail.com>
2022-03-05 21:07:11 +0330

    change app

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 flask db upgrade
-# gunicorn -w 4 --log-level debug --access-logfile ./acct.log -b 0.0.0.0:5000 "acct:create_app()"
-gunicorn -w 4 --log-level debug --access-logfile /dev/fd/1 -b 0.0.0.0:5000 "acct:create_app()"
+# gunicorn -w 4 --log-level debug --access-logfile ./acct.log -b 0.0.0.0:5000 "authman:create_app()"
+gunicorn -w 4 --log-level debug --access-logfile /dev/fd/1 -b 0.0.0.0:5000 "authman:create_app()"
